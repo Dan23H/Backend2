15eaadd6bbc452ba1d05386f6ac065c1248feea5
HexDan <daniel.hernandez_val@uao.edu.co>
2023-05-31 16:28:59 -0500

    modified:   database/config.js

--- database/config.js
+++ database/config.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-const dbConnection = async() => {
+const databaseConn = async() => {
     try {
         mongoose.connect(process.env.DB_CONNECTION, {
             autoIndex: true
         })
-        console.log('MongoDB is now Online')
+        console.log('MongoDB is Online')
     } catch(error) {
         console.log(error)
         throw new Error('Error connecting MongoDB')
     }
 }
-module.exports = { dbConnection }
+module.exports = { databaseConn }
